ba27ba6e7dd8e94d078fc68cba242aff64c3a992
Willian S <wsapcf@gmail.com>
2021-06-21 20:31:32 -0300

    create SplashScreen

---
+++ lib/pages/splash.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_images.dart';
+
+class Splash extends StatelessWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      body: Center(
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Image.asset(AppImages.logoFull),
+            Positioned(
+              child: Image.asset(AppImages.union),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
