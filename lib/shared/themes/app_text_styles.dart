f778b670215571bff99614914d519e7826c5148a
Willian S <wsapcf@gmail.com>
2021-06-21 20:18:48 -0300

    refactor file name of app_colors

--- lib/shared/themes/app_text_styles.dart
+++ lib/shared/themes/app_text_styles.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-import 'appcolors.dart';
+import 'app_colors.dart';
 
 class TextStyles {
   static final titleHome = GoogleFonts.lexendDeca(
