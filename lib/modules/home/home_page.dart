1c52784035c69e4fc5972dfc39d9b0561e2c35f0
Willian S <wsapcf@gmail.com>
2021-06-23 19:10:58 -0300

    add appBar header configuration

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,11 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/shared/auth/auth_controller.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final authController = new AuthController();
+
     return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(152),
+        child: Container(
+          height: 152,
+          color: AppColors.primary,
+          child: Center(
+            child: ListTile(
+              title: Text.rich(
+                TextSpan(
+                    text: "Olá ",
+                    style: TextStyles.titleRegular,
+                    children: [
+                      TextSpan(
+                          text: "Willian",
+                          style: TextStyles.titleBoldBackground)
+                    ]),
+              ),
+              subtitle: Text(
+                "Mantenha suas contas em dia",
+                style: TextStyles.captionShape,
+              ),
+              trailing: Container(
+                  height: 48,
+                  width: 48,
+                  decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(5))),
+            ),
+          ),
+        ),
+      ),
       body: Center(
         child: Text("Home Page"),
       ),
