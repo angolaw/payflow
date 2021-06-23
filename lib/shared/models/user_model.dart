2683ad42c847cf88c1812074ded015f9e332bf72
Willian S <wsapcf@gmail.com>
2021-06-23 18:50:44 -0300

    add fromMap factory

--- lib/shared/models/user_model.dart
+++ lib/shared/models/user_model.dart
@@ -8,4 +8,7 @@ class UserModel {
 
   Map<String, dynamic> toMap() => {"name": name, "photoURL": photoURL};
   String toJSON() => json.encode(toMap());
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(name: map['name'], photoURL: map['photoURL']);
+  }
 }
