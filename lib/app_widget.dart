1c4c79ab1eca20006ac823d2b01aa320010e9710
Willian S <wsapcf@gmail.com>
2021-06-23 17:22:33 -0300

    fix { missing

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -15,3 +15,4 @@ class AppWidget extends StatelessWidget {
       home: LoginPage(),
     );
   }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:payflow/shared/themes/app_colors.dart';
 
 import 'app_widget.dart';
-import 'modules/login/login_page.dart';
 
 void main() {
   runApp(AppWidget());
-}
-
 }
