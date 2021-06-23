03e1830dd9c76ac84ddb305e0393f54bbc7bfd90
Willian S <wsapcf@gmail.com>
2021-06-23 18:20:04 -0300

    refactor authController to use named routes

--- lib/shared/auth/auth_controller.dart
+++ lib/shared/auth/auth_controller.dart
@@ -11,12 +11,10 @@ class AuthController {
       _user = user;
 
       _isAuthenticated = true;
-      Navigator.pushReplacement(
-          context, MaterialPageRoute(builder: (context) => HomePage()));
+      Navigator.pushNamed(context, '/home');
     } else {
       _isAuthenticated = false;
-      Navigator.pushReplacement(
-          context, MaterialPageRoute(builder: (context) => LoginPage()));
+      Navigator.pushNamed(context, '/login');
     }
   }
 }
