7ffc7b8f1564d70d948f046dfe4205ef240a1e0c
Willian S <wsapcf@gmail.com>
2021-06-23 18:56:08 -0300

    remove unused variable

--- lib/shared/auth/auth_controller.dart
+++ lib/shared/auth/auth_controller.dart
@@ -5,17 +5,14 @@ import 'package:payflow/shared/models/user_model.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthController {
-  var _isAuthenticated = false;
   UserModel? _user;
   UserModel get user => _user!;
   void setUser(BuildContext context, var user) {
     if (user != null) {
       _user = user;
 
-      _isAuthenticated = true;
       Navigator.pushReplacementNamed(context, '/home');
     } else {
-      _isAuthenticated = false;
       Navigator.pushReplacementNamed(context, '/login');
     }
   }
