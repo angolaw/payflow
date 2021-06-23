b73e9593dd5a6534edeed36a153c3cef06a1fae1
Willian S <wsapcf@gmail.com>
2021-06-23 18:45:40 -0300

    add userModel

--- lib/shared/auth/auth_controller.dart
+++ lib/shared/auth/auth_controller.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:payflow/modules/home/home_page.dart';
 import 'package:payflow/modules/login/login_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthController {
   var _isAuthenticated = false;
@@ -16,5 +17,18 @@ class AuthController {
       _isAuthenticated = false;
       Navigator.pushReplacementNamed(context, '/login');
     }
+  }
+
+  Future<void> saveUser(var user) async {
+    final instance = await SharedPreferences.getInstance();
+    instance.setString("user", user);
+    return;
+  }
+
+  Future<void> currentUser(BuildContext context) async {
+    final instance = await SharedPreferences.getInstance();
+    final user = instance.get("user");
+    setUser(context, user);
+    return;
   }
 }
---
+++ lib/shared/models/user_model.dart
@@ -0,0 +1,6 @@
+class UserModel {
+  final String name;
+  final String photoURL;
+
+  UserModel({required this.name, required this.photoURL});
+}
