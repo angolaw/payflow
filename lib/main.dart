726279cf4d14bf4a65e0e9b19ce1f9cc39da1eab
Willian S <wsapcf@gmail.com>
2021-06-23 17:21:31 -0300

    move AppWidget to its own file

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 
+import 'app_widget.dart';
 import 'modules/login/login_page.dart';
 
 void main() {
   runApp(AppWidget());
 }
 
-class AppWidget extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'PayFlow',
-      theme: ThemeData(
-        primaryColor: AppColors.primary,
-      ),
-      home: LoginPage(),
-    );
-  }
 }
