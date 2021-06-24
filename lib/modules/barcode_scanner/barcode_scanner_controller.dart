dc7687bb21c725816a52ede8be975ffc7b31afdc
Willian S <wsapcf@gmail.com>
2021-06-24 10:02:49 -0300

    use named constructors on barcodeScannerController:

--- lib/modules/barcode_scanner/barcode_scanner_controller.dart
+++ lib/modules/barcode_scanner/barcode_scanner_controller.dart
@@ -1,9 +1,18 @@
 import 'package:camera/camera.dart';
+import 'package:payflow/modules/barcode_scanner/barcode_scanner_status.dart';
 
 class BarcodeScannerController {
+  BarcodeScannerStatus status = BarcodeScannerStatus();
   void getAvailableCameras() async {
-    final response = await availableCameras();
-    final camera = response.firstWhere(
-        (element) => element.lensDirection == CameraLensDirection.back);
+    try {
+      final response = await availableCameras();
+      final camera = response.firstWhere(
+          (element) => element.lensDirection == CameraLensDirection.back);
+      final cameraController =
+          CameraController(camera, ResolutionPreset.max, enableAudio: false);
+      status = BarcodeScannerStatus.availableCamera(cameraController);
+    } catch (e) {
+      status = BarcodeScannerStatus.error(e.toString());
+    }
   }
 }
